f72fb1976d3c069982209bda18e5aa82116f92ba
MG <168362033+MG-Corp@users.noreply.github.com>
2024-04-30 02:39:06 +0000

    v1.0BETA

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # MScript
+v1.0BETA 
+## Documentation
+MScript is extremely similiar to Python in syntax and features; however, there are some differences:
+- `def` -> `func`
+- `and` -> `&`
+- `or` -> `|`
+- `match` -> `switch`
+<br />
+There is two versions of print: `println` and `print`. <br />
+`println` Automatically prints a value on a newline <br />
+`print` Prints very value on the same line
---
+++ mscript.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def read(file: str):
+    with open(file, "r") as f:
+        data = f.read()
+
+    return data
+
+def mscrToPy(fileName: str):
+    file = read(fileName)
+
+    mprint = file.replace("println(", "print(")
+    mprint2 = mprint.replace('")', '\\n")')
+
+    python = mprint2.replace("func", "def").replace("&", "and").replace("|", "or").replace("switch", "match")
+
+    with open(f"{file}.py", "w") as i:
+        i.write(str(python))
+
+    return file
